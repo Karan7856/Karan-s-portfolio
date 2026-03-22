9254a0ec83a13fd8076902ba7b0dee3222e63ff2
Karan <kr893781@gmail.com>
2026-03-22 23:42:27 +0530

    fix: scroll to top on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 
+// Always start from the top on refresh
+if ('scrollRestoration' in history) history.scrollRestoration = 'manual'
+window.scrollTo(0, 0)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
